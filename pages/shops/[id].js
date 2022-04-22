5e58c9e605e4a7399df3ee46d2a2048ed67905f8
Luigi  C. Campagnola <luigi.campagnola@hnddev01.local>
2022-04-22 10:01:00 -0600

    adding changes to [id].js and setting dynamic paths

--- pages/shops/[id].js
+++ pages/shops/[id].js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "../../styles/coffee-store.module.css";
 
 import coffeeStoreData from "../../data/coffee-stores.json";
 
@@ -9,55 +10,53 @@ export async function getStaticProps(staticProps) {
   return {
     props: {
       coffeeStore: coffeeStoreData.find((coffeeStore) => {
-        return coffeeStore.id === 0; //dynamic id
+        return coffeeStore.id.toString() === params.id; //dynamic id
       }),
     },
   };
 }
 
 export function getStaticPaths() {
+  const paths = coffeeStoreData.map((coffeeStore) => {
+    console.log(coffeeStore);
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
   return {
-    paths: [
-      {
-        params: {
-          id: "0",
-        },
-      },
-      {
-        params: {
-          id: "1",
-        },
-      },
-      {
-        params: {
-          id: "300",
-        }
-      }
-    ],
-    fallback: false,
+    paths,
+    fallback: true,
   };
 }
 
 export default function Shops(props) {
-  console.log("props", props)
+  console.log("props", props);
   const router = useRouter();
   console.log("router", router);
+
+  if (router.isFallback) {
+    return <div>Loading..</div>;
+  }
+  const { address, name, neighbourhood } = props.coffeeStore; // destructured props
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
-        <title>{router.query.id}</title>
+        <title>{name}</title>
       </Head>
-      <h1>{router.query.id}</h1>
       <Link href="/">
         <a>
           <button>Go Back</button>
         </a>
       </Link>
-      <p>{props.coffeeStore.name}</p>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.neighbourhood}</p>
-
-
+      <div className={styles.container}>
+        <div className={styles.nameWrapper}>
+          <p className={styles.name}>{name}</p>
+        </div>
+        <p className={styles.text}>{address}</p>
+        <p className={styles.text}>{neighbourhood}</p>
+      </div>
     </div>
   );
 }
